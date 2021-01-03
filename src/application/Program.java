24463e1ca2a72cffdeee1b2ddea6f007301739e9
Everton Godinho <66344588+efgtheone@users.noreply.github.com>
2021-01-03 14:21:42 -0300

    Projeto criado

---
+++ src/application/Program.java
@@ -0,0 +1,27 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		System.out.println("Entre three numbers: ");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		
+		if(a>b &&a>c) {
+			System.out.println("Higher = "+a);
+		}
+		else if (b>a && b>c) {
+			System.out.println("Higher = "+b);
+			
+		}else {
+			System.out.println("Higher = "+c);
+		}
+		sc.close();
+
+	}
+
+}
